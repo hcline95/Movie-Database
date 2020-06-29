2321486a953600d79ac105eb3b7757014fd39cd9
Hannah Cline <hphuston0212@email.campbell.edu>
2020-06-28 22:00:51 -0400

    changed the initial state of total pages

--- server/client/src/reducers/reducer-total-pages.js
+++ server/client/src/reducers/reducer-total-pages.js
@@ -1,6 +1,6 @@
 import { FETCH_MOVIES, ERROR_MESSAGE } from '../actions/types';
 
-export default function(state = 0, action) {
+export default function(state = null, action) {
   switch (action.type) {
     case FETCH_MOVIES:
       return action.payload.data.total_pages
